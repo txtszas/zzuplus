bfe6b49158c273735b73211bc0be754d4f12196b
txtszas <txtszas@gmail.com>
2012-10-21 23:29:21 +0800

    build first rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zzuplus::Application.config.secret_token = '6aa647f08347e643167f46bcad0c3f39da21fa3f8becbfe57430a1a92739254ac7c8c52ab9161d62ea81b5be34d386e4cb4c0b51178ea5a9241c01829a5548d5'
