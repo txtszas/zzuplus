3157b1ac5b2bba14702cd9735b699c051d99bff6
txtszas <txtszas@gmail.com>
2012-10-22 00:49:18 +0800

    add answer model and controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
   attr_accessible :content
+  
+  validates :content, :presence => true
+  
+  has_many  :answers
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Zzuplus::Application.routes.draw do
-  resources :questions
+  resources :questions do
+      resources :answers
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
